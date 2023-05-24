7264d86152bb6cd62ead71b6d8965f74e9f3b6fc
jmxbgrn <134468297+jmxbgrn@users.noreply.github.com>
2023-05-24 11:29:48 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import tensorflow as tf
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = tf.keras.models.load_model('dog_model.h5')
+    return model
+
+model = load_model()
+
+st.write("# Dog Breed Classification System by Jay Merie Ann Bugarin")
+
+file = st.file_uploader("Choose any dog photo from computer", type=["jpg", "png"])
+
+def import_and_predict(image_data, model):
+    size = (244, 244)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    image = ImageOps.grayscale(image)
+    img = np.asarray(image)
+    img = img.reshape((size[0], size[1], 1))
+    img_reshape = img[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
+    return prediction
+
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    class_names = ['African hunting dog','Chow Chow','French bulldog','German shepherd','Pomeranian','Poodle','Pug', 'Rottweiler', 'Tibetan mastiff', 'Yorkshire terrier']
+    string = "OUTPUT: " + class_names[np.argmax(prediction)]
+    st.success(string)
